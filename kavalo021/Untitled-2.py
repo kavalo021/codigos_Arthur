88ce407a70184b689eeeaf71ba6c38fd1a8b0bf6
Athurzin_021 <kavalousd@gmail.com>
2025-08-28 15:49:23 -0300

    Add files via upload

---
+++ kavalo021/Untitled-2.py
@@ -0,0 +1,33 @@
+print("Arthur"*8000)
+#exenplo de variavel
+fruta = "manga" 
+print (type(fruta))
+#exsemplo variavel do tipo INT é inteiro, numeros inteiros
+idade = int(35)
+#exsemplo de numero quebrado, FLOAT
+nota = flot(9.5)
+
+#  boleana
+vc_curte = false 
+
+### EXCICIO 1
+
+# crar uma variavel que peça o nome do usuario 
+# depois criar uma variavel do tipo float, que pede a nota do usuario 
+# depois criar uma variavel a idade do usuario
+#depois de um print nas suas variaveis.
+nome = imput("didite seu nome")
+print(nome)
+nota = _float("input("digite sua nota :")")
+print(nota)
+print = float("input(digite sua idade)")
+print(idade)
+
+### Exercio 2
+### crie uma variavel que se chama nome_carro, coloque nessa variavel o nome dos seu sonhos
+### crie uma variavel que se chama preço,coque dentro dessa variavel o preço do carro
+### crie uma variavel que se chama ano,coleque ano do carro 
+
+nome_carro = ("lanborguini veiron")
+preço_carro =float(1.000.000)
+ano_carro =int(2029)
